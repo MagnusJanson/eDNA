b43e19c348683bdcc19a2c6c898daaf4460fb21d
MagnusJanson <85447588+MagnusJanson@users.noreply.github.com>
2023-05-26 16:30:59 +0100

    Update eDNA_sed_rarefaction_curve.R

--- eDNA_sed_rarefaction_curve.R
+++ eDNA_sed_rarefaction_curve.R
@@ -1,24 +1,17 @@
+install.packages("vegan")
 library(vegan)
 library(readxl)
 sedi <- read_excel("C:/Users/magnu/Downloads/eDNA_results_sediment_for_analysis.xlsx")
 as.data.frame(sedi)
-sedi_no_site <- sedi[, -1] #Take away the site column to only keep numerical values
-rarefaction_curve <- specaccum(sedi_no_site, method = "rarefaction")
-plot(rarefaction_curve, ci.type = "polygon", ci.col = "gray", lwd = 2,
-     xlab = "Sequences per Sample", ylab = "Operational Taxonomic Units (OTUs)",
-     main = "Rarefaction Curve")
+sedi_no_site <- sedi[, -1] #Take away the site column
 
 
+#Perform the rarefaction curve
+data(sedi_no_site) #
+S <- specnumber(sedi_no_site) # observed number of species
+(raremax <- min(rowSums(sedi_no_site)))
+Srare <- rarefy(sedi_no_site, 1000)
+plot(S, Srare, xlab = "Observed No. of Species", ylab = "Rarefied No. of Species")
+abline(0, 1)
+a<-rarecurve(sedi_no_site, step = 20, sample = raremax, col = "blue", cex = 0.6)
 
-
-#Trying to perform rarefaction curves on different species
-Cliona <- sedi_no_site[, "Cliona", drop = FALSE]
-as.data.frame(Cliona)
-Enhydrosoma_propinquum <- sedi_no_site[, "Enhydrosoma propinquum", drop = FALSE]
-Edwardsia_longicornis <- sedi_no_site[, "Edwardsia longicornis", drop = FALSE]
-Bougainvillia <- sedi_no_site[, "Bougainvillia", drop = FALSE]
-major_species <- cbind(Cliona,Enhydrosoma_propinquum,Edwardsia_longicornis,Bougainvillia)
-rarefaction_curve <- specaccum(major_species, method = "rarefaction")
-plot(rarefaction_curve, ci.type = "polygon", ci.col = "gray", lwd = 2,
-     xlab = "Sequences per Sample", ylab = "Operational Taxonomic Units (OTUs)",
-     main = "Rarefaction Curve")
